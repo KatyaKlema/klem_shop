524ea77866bb5c032c5215a248c775c886ede1f0
Клементьева Екатерина Николаевна <enklemen@www.mts.ru>
2021-04-23 19:57:07 +0300

    brute force

---
+++ src/components/Directory/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import woman from "./../../assets/woman.jpg";
+import men from "./../../assets/men.jpg";
+import "./styles.scss";
+
+const Directory = (props) => {
+  return (
+    <div className="directory">
+      <div className="wrap">
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${woman})`,
+          }}
+        >
+          <a>Shop Women</a>
+        </div>
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${men})`,
+          }}
+        >
+          <a>Shop Men</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Directory;
