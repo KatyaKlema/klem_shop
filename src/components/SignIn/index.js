d101544de49a16bb4183910b392ed29ecc6738c0
Клементьева Екатерина Николаевна <enklemen@www.mts.ru>
2021-04-23 23:52:26 +0300

    add login and registration

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,21 +1,66 @@
 import React, { Component } from "react";
 import "./styles.scss";
 import Button from "./../forms/Button";
-import { signInWithGoogle } from "../../firebase/utils";
+import { auth, signInWithGoogle } from "../../firebase/utils";
+import FormInput from "../forms/FormInput";
 
+const initialState = {
+  email: "",
+  password: "",
+  error: "",
+};
 class SignIn extends Component {
-  handleSubmit = async (e) => {
-    e.preventDefault();
+  constructor(props) {
+    super(props);
+    this.state = { ...initialState };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ ...initialState });
+    } catch (err) {
+      this.setState({ error: err.message });
+      console.log(err);
+    }
   };
 
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password, error } = this.state;
     return (
-      <div className="signin">
+      <div className="signIn">
         <div className="wrap">
           <h2>LogIn</h2>
-
+          {error}
           <div className="formWrap">
             <form onSubmit={this.handleSubmit}>
+              <FormInput
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                handleChange={this.handleChange}
+              />
+              <FormInput
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Password"
+                handleChange={this.handleChange}
+              />
+
+              <Button type="submit">LogIn</Button>
+
               <div className="socialSignIn">
                 <div className="row">
                   <Button onClick={signInWithGoogle}>
