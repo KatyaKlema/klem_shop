37e828254734ad94f0f23d79990d852d9c746c0b
Клементьева Екатерина Николаевна <enklemen@www.mts.ru>
2021-04-24 19:09:43 +0300

    add redux hooks

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Logo from "./../../assets/logo.png";
-import "./styles.scss";
 import { auth } from "../../firebase/utils";
+import "./styles.scss";
+
+const mapState = ({ user }) => ({
+  currentUser: user.currentUser,
+});
 
 const Header = (props) => {
-  const { currentUser } = props;
+  const { currentUser } = useSelector(mapState);
   return (
     <header className="header">
       <div className="wrap">
@@ -46,7 +50,4 @@ Header.defaultProps = {
   currentUser: null,
 };
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
-});
-export default connect(mapStateToProps, null)(Header);
+export default Header;
