c7bdd79d804d2902215740c8afb45271d6a2039d
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2021-07-24 13:57:20 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyA6LYhxetm2EuFIEvPFTXrmHpdirMiVneg",
-  authDomain: "eklemshop.firebaseapp.com",
-  projectId: "eklemshop",
-  storageBucket: "eklemshop.appspot.com",
-  messagingSenderId: "839040857694",
-  appId: "1:839040857694:web:db8840c69d95db7ce826cf",
-  measurementId: "G-9VT1THKHGM",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
